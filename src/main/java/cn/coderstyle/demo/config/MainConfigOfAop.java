18623357514f10ce29c0d2acd5cd418779cf4bec
jinggj <jinggj@lenovocloud.com>
2018-12-18 17:59:45 +0800

    aop022

--- src/main/java/cn/coderstyle/demo/config/MainConfigOfAop.java
+++ src/main/java/cn/coderstyle/demo/config/MainConfigOfAop.java
@@ -52,6 +52,18 @@ import cn.coderstyle.demo.aop.MathCalculator;
  * 								.postProcessxxx() 后置处理器的实现
  * 		AbstractAdvisorAutoProxyCreator	
  * 					
+ * 	流程：
+ * 		1.传入配置类，创建ioc容器;
+ * 		2.注册撇脂类，调用refresh() 刷新容器;
+ * 		3.registerBeanPostProcessors(beanFactory);注册bean的后置处理器来拦截bean的创建
+ * 			1）、先获取ioc容器已经定义了的需要创建对象的所有BeanPostProcessor
+ * 			2）、给容器中加别的BeanPostProcessor
+ * 			3）、优先注册实现了PriorityOrdered接口的BeanPostProcessor
+ * 			4）、再给容器中注册实现了Ordered接口的BeanPostProcessor
+ * 			5）、注册没实现优先级接口的BeanPostProcessor
+ * 			6）、注册BeanPostProcessor，实际上创建BeanPostProcessor对象，保存再容器中
+ * 				创建internalAutoProxyCreator=AnnotationAwareAspectJAutoProxyCreator
+ * 
  * @author champion
  *
  */
