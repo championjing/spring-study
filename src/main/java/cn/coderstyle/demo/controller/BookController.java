4e76512617362a77a40aa1183da5a41fab3d8acc
champion <championjing@163.com>
2019-02-26 20:12:13 +0800

    参数获取方式

--- src/main/java/cn/coderstyle/demo/controller/BookController.java
+++ src/main/java/cn/coderstyle/demo/controller/BookController.java
@@ -1,11 +1,14 @@
 package cn.coderstyle.demo.controller;
 
+import org.aspectj.weaver.tools.Trace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import cn.coderstyle.demo.service.BookService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,15 @@ public class BookController {
 	public String print() {
 		System.out.println("controller:"+bookService2);
 		return "hello";
+	}
+	/**
+	 * 当时直接使用方法中参数方式获取request中的参数，这种形式相当于@RequestParam(name="abc",required = false)
+	 * @param abc
+	 */
+	@RequestMapping(value="/test",method=RequestMethod.POST)
+	public void testParam( @RequestParam(name="abc")String abc) {
+		
+		System.out.println("abc:"+abc);
+		
 	}
 }
